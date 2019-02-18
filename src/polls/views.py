8d400ff619e861a20ef23f9595fc792bb5e2b13d
Stephan <stephanedmonson@gmail.com>
2019-02-17 20:43:58 -0500

    completed step 3
    wrote views
    
    used render and get_object_or_404 method
    
    learned tags and variables
    
    namespaced url names

--- src/polls/views.py
+++ src/polls/views.py
@@ -1,6 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse, Http404
+from django.template import loader
 
-from django.http import HttpResponse
+from .models import Question
 
+#Old HttpResponse way
+# def index(request):
+    # latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # template = loader.get_template('polls/index.html')
+    # context = {
+    #     'latest_question_list': latest_question_list,
+    # }
+    #return HttpResponse(template.render(context, request))
+    # shortcut way using render
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+    # shortcut for above method uses get_object_or_404 method
+    # essentially replaces need to write an error and simplifies the check
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
