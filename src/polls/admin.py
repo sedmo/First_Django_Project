1818ecaa6a879a9a38711a0aed6e01b1b31df03e
Stephan <stephanedmonson@gmail.com>
2019-02-18 01:53:01 -0500

    Tutorial completed. Last part included customization for the admin site

--- src/polls/admin.py
+++ src/polls/admin.py
@@ -1,6 +1,27 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Question
+from .models import Choice, Question
 
-admin.site.register(Question)
+
+# This tells Django: “Choice objects are 
+# edited on the Question admin page. 
+# By default, provide enough fields for 3 choices.”
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 1
+
+# btw, create a model admin class any time
+# you need to change the admin options for a model.
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')  
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+    
+admin.site.register(Question, QuestionAdmin)
